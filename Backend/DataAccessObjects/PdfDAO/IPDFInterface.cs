39df534b4d7159246b6de5243d4f13c2fefd8cbe
baizi <baizidkhaled@yahoo.fr>
2024-05-04 08:48:29 +0200

    Backend_ Delete PDF file.

--- Backend/DataAccessObjects/PdfDAO/IPDFInterface.cs
+++ Backend/DataAccessObjects/PdfDAO/IPDFInterface.cs
@@ -1,6 +1,10 @@
-﻿namespace Backend.DataAccessObjects.PdfDAO;
+﻿using Shared;
+
+namespace Backend.DataAccessObjects.PdfDAO;
 
 public interface IPDFInterface
 {
     public Task<string> AddPDFAsync(string url);
+    public Task<bool> IsPDFExistAsync(string url);
+    public Task<string> DeletePDFAsync(string url);
 }
--- TestProject/PDFServiceTests.cs
+++ TestProject/PDFServiceTests.cs
@@ -9,30 +9,23 @@ namespace TestProject;
 
 public class PDFServiceTests
 {
-
+ string apiKey = "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";
     
-   // public IEmbeddingProvider EmbeddingProvider => _embeddingProvider;
-
-    // public PDFServiceTests(IEmbeddingProvider embeddingProvider, IPromptProvider promptProvider)
-    // {
-    //     _embeddingProvider = embeddingProvider;
-    //     _promptProvider = promptProvider;
-    // }
-
     [Test]
     public async Task ShouldGetConfirmationMessage_WithAddingPDF_WhenTheUrlHasValidPDFExtension()
     {
     
         // Arrange
         var context = new DataContext(new ConfigurationBuilder().Build(), useInMemoryDatabase: true);
-       // await context.PDFs.AddAsync(new PDF {  Url="C:\\Users\\baizi\\Desktop\\Boyum\\Khaled.pdf", AdminId = 1 }); 
-       // await context.SaveChangesAsync();
-        var service = new PDFImplementation(context,new EmbeddingProvide("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"),new PromptProvider(new EmbeddingProvide("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")));
-    
+        await context.Database.EnsureCreatedAsync();
+        var service = new PDFImplementation(context,new EmbeddingProvide(apiKey),new PromptProvider(new EmbeddingProvide(apiKey)));
+        var url = "https://www.plainenglish.co.uk/files/formsguide.pdf";
         // Act
-        var result = await service.AddPDFAsync("https://www.plainenglish.co.uk/files/formsguide.pdf");
-    
+        var result = await service.AddPDFAsync(url);
+        var isAdded = await service.IsPDFExistAsync(url);
+        
         // Assert
+        Assert.That(isAdded,Is.True);
         Assert.That(result, Is.Not.Null);
         Assert.That(result, Is.EqualTo("pdf is added successfully"));
         await context.Database.EnsureDeletedAsync();
@@ -44,18 +37,58 @@ public class PDFServiceTests
     
         // Arrange
         var context = new DataContext(new ConfigurationBuilder().Build(), useInMemoryDatabase: true);
-        // await context.PDFs.AddAsync(new PDF {  Url="C:\\Users\\baizi\\Desktop\\Boyum\\Khaled.pdf", AdminId = 1 }); 
-        // await context.SaveChangesAsync();
-        var service = new PDFImplementation(context,new EmbeddingProvide("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"),new PromptProvider(new EmbeddingProvide("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")));
-    
+        await context.Database.EnsureCreatedAsync();
+        var service = new PDFImplementation(context,new EmbeddingProvide(apiKey),new PromptProvider(new EmbeddingProvide(apiKey)));
+        var url = "https://www.plainenglish.co.uk/files/formsguide.txt";
         // Act
-        var result = await service.AddPDFAsync("test.txt");
-    
+        var result = await service.AddPDFAsync(url);
+        var isAdded = await service.IsPDFExistAsync(url);
         // Assert
+        Assert.That(isAdded,Is.False);
         Assert.That(result, Is.Not.Null);
         Assert.That(result, Is.EqualTo("The url does not have a pdf extension, please provide a valid pdf url with valid extension"));
         await context.Database.EnsureDeletedAsync();
     }
-
+ 
+    [Test]
+    public async Task ShouldGetConfirmationMessage_WhenDeletingPDF_WithAValidExistingURL()
+    {
+    
+        // Arrange
+        var context = new DataContext(new ConfigurationBuilder().Build(), useInMemoryDatabase: true);
+        await context.Database.EnsureCreatedAsync();
+        var service = new PDFImplementation(context,new EmbeddingProvide(apiKey),new PromptProvider(new EmbeddingProvide(apiKey)));
+        var url = "https://www.plainenglish.co.uk/files/formsguide.pdf";
+        await service.AddPDFAsync(url);
+        // Act
+     var  result = await service.DeletePDFAsync(url);
+        var isExisting = await service.IsPDFExistAsync(url);;
+        
+        // Assert
+        Assert.That(isExisting,Is.False);
+        Assert.That(result, Is.EqualTo("pdf is deleted successfully"));
+        await context.Database.EnsureDeletedAsync();
+    }
+    
+    [Test]
+    public async Task ShouldGetErrorMessage_WhenDeletingPDF_WithNoValidExistingURL()
+    {
+    
+        // Arrange
+        var context = new DataContext(new ConfigurationBuilder().Build(), useInMemoryDatabase: true);
+        await context.Database.EnsureCreatedAsync();
+        var service = new PDFImplementation(context,new EmbeddingProvide(apiKey),new PromptProvider(new EmbeddingProvide(apiKey)));
+        var url = "https://www.plainenglish.co.uk/files/formsguide.pdf";
+        var notExistingUrl = "https://www.plainenglish.co.uk/files/formsguide1.pdf";
+        await service.AddPDFAsync(url);
+        // Act
+        var  result = await service.DeletePDFAsync(notExistingUrl);
+        var isExisting = await service.IsPDFExistAsync(url);;
+        
+        // Assert
+        Assert.That(isExisting,Is.True);
+        Assert.That(result, Is.EqualTo("pdf is not found"));
+        await context.Database.EnsureDeletedAsync();
+    }
     
 }
